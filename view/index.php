4bb06e2275f95df9fa9b5cd5305763b8ab2e34fc
madfrogf1 <madfrogf1@gmail.com>
2015-04-14 13:10:56 +0930

    draft

---
+++ view/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+
+    <script src="../asset/js/html5shiv/html5shiv.min.js"></script>
+    <script src="../asset/js/html5shiv/html5shiv-printshiv.min.js"></script>
+
+    <title></title>
+
+    <link rel="stylesheet" href="../asset/css/normalize/normalize.css">
+    <link rel="stylesheet" href="../asset/css/index.css">
+    <link rel="stylesheet" href="../asset/css/header/header.css">
+    <link rel="stylesheet" href="../asset/css/advertisement/advertisement.css">
+    <link rel="stylesheet" href="../asset/css/btGroupLink/btGroupLink.css">
+    <link rel="stylesheet" href="../asset/css/btGroupLink/btLogistics/btLogistics.css">
+    <link rel="stylesheet" href="../asset/css/btGroupLink/btSupplyChainSolutions/btSupplyChainSolutions.css">
+    <link rel="stylesheet" href="../asset/css/btGroupLink/btTransport/btTransport.css">
+    <link rel="stylesheet" href="../asset/css/btGroupLink/btWarehousing/btWarehousing.css">
+    <link rel="stylesheet" href="../asset/css/footer/footer.css">
+
+</head>
+
+<body>
+    <div id = "container">
+        <div id = "header">
+         </div>
+        
+        <div id = "advertise">
+        </div>
+        
+        <div id = "group">
+        </div>
+        
+        <div id = "footer">
+        </div>
+        
+    </div>
+	<!-- here is body content -->
+
+
+    <script src="../asset/js/index.js"></script>
+    <script src="../asset/js/jQuery/jquery-1.11.1.min.js"></script>
+    <script src="../asset/js/header/header.js"></script>
+    <script src="../asset/js/advertisement/advertisement.js"></script>
+    <script src="../asset/js/btGroupLink/btGroupLink.js"></script>
+    <script src="../asset/js/btGroupLink/btLogistics/btLogistics.js"></script>
+    <script src="../asset/js/btGroupLink/btSupplyChainSolutions/btSupplyChainSolutions.js"></script>
+    <script src="../asset/js/btGroupLink/btTransport/btTransport.js"></script>
+    <script src="../asset/js/btGroupLink/btWarehousing/btWarehousing.js"></script>
+    <script src="../asset/js/footer/footer.js"></script>
+
+</body>
+
+</html>
